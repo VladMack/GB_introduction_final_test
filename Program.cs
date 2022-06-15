e9d9694000cd806a89d9da2daade132610efaca4
Vlad M <vlad.mackutchev@gmail.com>
2022-06-15 14:39:30 +0300

    add GetLenSelectedArray method

--- Program.cs
+++ Program.cs
@@ -8,3 +8,16 @@ string[] array = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEnt
 string[] resultArray = GetLenSelectedArray(array, 3);
 
 Console.WriteLine(string.Join(", ", resultArray));
+
+string[] GetLenSelectedArray(string[] arr, int charNumber)
+{
+    string[] result = new string[arr.Count(x => x.Length <= charNumber)];
+    int count = 0;
+    foreach (string el in arr)
+        if (el.Length <= charNumber)
+        {
+            result[count] = el;
+            count++;
+        }
+    return result;
+}
