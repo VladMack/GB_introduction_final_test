33e21f5d9029c6cbf5c282747a84ecbc5f7b05d7
Vlad M <vlad.mackutchev@gmail.com>
2022-06-15 14:36:35 +0300

    add main code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+
+Console.Clear();
+
+Console.WriteLine("Введи элементы массива через пробел:");
+string[] array = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+string[] resultArray = GetLenSelectedArray(array, 3);
+
+Console.WriteLine(string.Join(", ", resultArray));
