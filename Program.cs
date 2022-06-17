8c0f43a486c1943cb3da8faff8678188a70f1969
Vlad M <vlad.mackutchev@gmail.com>
2022-06-17 11:35:57 +0300

    edit code

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 Console.Clear();
 
 Console.WriteLine("Введи элементы массива через пробел:");
-string[] array = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+string[] array = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
 string[] resultArray = GetLenSelectedArray(array, 3);
 
 Console.WriteLine(string.Join(", ", resultArray));
